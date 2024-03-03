7864d267016a22aa1e9986820ca4d13dbd9fcdff
Daniel Bray <daniel.bray.dev@hotmail.com>
2024-03-03 23:31:24 +0000

    Add test cases for start command

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
 import os
 import json
 import shutil
+import hashlib
 
 from .paths import paths
 
@@ -46,3 +47,57 @@ def copy_file(source, destination):
     with open(source, "rb") as src_file:
         with open(destination, "wb") as dest_file:
             dest_file.write(src_file.read())
+
+
+def copy_folder(source, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for item in os.listdir(source):
+        source_item = os.path.join(source, item)
+        destination_item = os.path.join(destination, item)
+        if os.path.isdir(source_item):
+            shutil.copytree(source_item, destination_item)
+        else:
+            shutil.copy2(source_item, destination_item)
+
+
+def calculate_md5(file_path):
+    md5_hash = hashlib.md5()
+    with open(file_path, "rb") as f:
+        for byte_block in iter(lambda: f.read(4096), b""):
+            md5_hash.update(byte_block)
+    return md5_hash.hexdigest()
+
+
+def get_all_files(path, relative_to=None):
+    file_paths = []
+    # Walk through the directory tree
+    for root, directories, files in os.walk(path):
+        # Append file paths to the list
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            if relative_to:
+                file_path = os.path.relpath(file_path, relative_to)
+            file_paths.append(file_path)
+
+    return file_paths
+
+
+def compare_folders(folder1, folder2):
+    files1 = get_all_files(folder1, folder1)
+    files2 = get_all_files(folder2, folder2)
+
+    if len(files1) != len(files2):
+        return False
+
+    if files1 != files2:
+        return False
+
+    for file in files1:
+        md5_1 = calculate_md5(os.path.join(folder1, file))
+        md5_2 = calculate_md5(os.path.join(folder2, file))
+        if md5_1 != md5_2:
+            return False
+
+    return True
---
+++ tests/testStartCommand.py
@@ -0,0 +1,149 @@
+import pytest
+
+import os
+import subprocess
+
+from src.paths import paths
+from src.helpers import *
+
+
+def test_copy_test_case():
+    copy_folder("tests/testStartCommandData/TestCase1", "./")
+
+    assert os.path.exists("./source/"), f"Folder 'source' does exist"
+    assert os.path.exists("./replica/"), f"Folder 'replica' does exist"
+
+
+def test_calculate_md5():
+    md5 = calculate_md5("tests/testStartCommandData/uniqueFiles/1.txt")
+
+    assert md5 == "d1965bbe240fb008bce41d8b7ce96335", "Md5 Checksum does not match"
+
+
+def test_get_all_files():
+    copy_folder("tests/testStartCommandData/TestCase1", "./")
+
+    files = get_all_files("./source")
+
+    expected = [
+        "./source/1.txt",
+        "./source/a/2.txt",
+        "./source/a/b/3.txt",
+        "./source/c/4.txt",
+        "./source/c/d/e/5.txt",
+    ]
+
+    assert files == expected, "Files are not equal"
+
+
+def test_get_all_files2():
+    copy_folder("tests/testStartCommandData/TestCase1", "./")
+
+    filesSource = get_all_files("./source/", relative_to="./source/")
+    filesReplica = get_all_files("./replica/", relative_to="./replica/")
+
+    assert filesSource == filesReplica, "Files are not equal"
+
+
+def test_compare_folders():
+    copy_folder("tests/testStartCommandData/TestCase1", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+# Same MD5, No folders
+
+
+def test_sync_1():
+    copy_folder("tests/testStartCommandData/TestCase2", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_2():
+    copy_folder("tests/testStartCommandData/TestCase3", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_3():
+    copy_folder("tests/testStartCommandData/TestCase4", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_4():
+    copy_folder("tests/testStartCommandData/TestCase5", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+# Same MD5, With folders
+
+
+def test_sync_5():
+    copy_folder("tests/testStartCommandData/TestCase6", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_6():
+    copy_folder("tests/testStartCommandData/TestCase7", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_7():
+    copy_folder("tests/testStartCommandData/TestCase8", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_8():
+    copy_folder("tests/testStartCommandData/TestCase9", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+# Different MD5
+
+
+def test_sync_9():
+    copy_folder("tests/testStartCommandData/TestCase10", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_10():
+    copy_folder("tests/testStartCommandData/TestCase11", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_11():
+    copy_folder("tests/testStartCommandData/TestCase12", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+def test_sync_12():
+    copy_folder("tests/testStartCommandData/TestCase13", "./")
+
+    assert compare_folders("./source/", "./replica/"), "Folders are not equal"
+
+
+# Do before and after tests
+
+paths_to_clean = [
+    paths["config"],
+    paths["defaultSource"],
+    paths["defaultReplica"],
+    paths["defaultLog"],
+]
+
+
+@pytest.fixture(autouse=True)
+def before_and_after_test():
+    clean_up_all(paths_to_clean)
+    yield
+    clean_up_all(paths_to_clean)
