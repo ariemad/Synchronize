1dd00553ac2ca1f5c5713575beeaea2dffb5c7f3
Daniel Bray <daniel.bray.dev@hotmail.com>
2024-03-04 15:06:18 +0000

    Add docstrings

--- synchronize.py
+++ synchronize.py
@@ -12,39 +12,62 @@ app = typer.Typer()
 @app.command()
 def start(
     repeat: bool = typer.Option(
-        False, "--repeat", "-r", help="Repeat according to interval "
+        False,
+        "--repeat",
+        "-r",
+        help="Runs the synchronization tool periodically, according to time interval ",
     ),
     print: bool = typer.Option(
-        False, "--print", "-p", help="Prints log to the terminal "
+        False, "--print", "-p", help="Prints the logs to the terminal "
     ),
 ):
+    """
+    Runs the synchronization script.
+    """
     command.validate()
     command.start(repeat, print)
 
 
 @app.command()
 def validate():
+    """
+    Validates the config file.
+    Informs the user if there is any incorrect value.
+    """
     command.validate()
     print("Config is valid.")
 
 
 @app.command()
 def showlog():
+    """
+    Shows logs.
+    """
     command.showLog()
 
 
 @app.command()
 def showconfig():
+    """
+    Shows config.
+    """
     command.showConfig()
 
 
 @app.command()
 def set(
-    source: str = typer.Option(None, "--source", help="Source path"),
-    replica: str = typer.Option(None, "--replica", help="Replica path"),
-    log: str = typer.Option(None, "--log", help="Logs path"),
-    interval: int = typer.Option(None, "--interval", help="Interval"),
+    source: str = typer.Option(None, "--source", help="Defines the source path"),
+    replica: str = typer.Option(None, "--replica", help=" Defines the replica path"),
+    log: str = typer.Option(None, "--log", help="Defines the log path"),
+    interval: int = typer.Option(
+        None,
+        "--interval",
+        help="Defines the synchronization interval in seconds (minimum of 10 seconds)",
+    ),
 ):
+    """
+    Changes a value in the config.
+    """
     command.set(source, replica, log, interval)
 
 
